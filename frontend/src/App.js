97c79a7bd12e3baf3d46d755412daca2a3c3a2e4
mekigiwi <danyagenij@gmail.com>
2025-03-08 16:27:05 +0300

    added registration form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,37 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import Registerform from './components/Registerform';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-// frontend/src/App.js
-
-function App() {
-  useEffect(() => {
-    axios.get('/api/hello/')
-      .then(response => console.log(response.data))
-      .catch(error => console.error('Ошибка:', error));
-  }, []);
-
-  return <div>Проверка интеграции</div>;
+export default function App() {
+  return (
+    <div className='App'>
+      <Registerform/>
+    </div>
+  );
 }
-
-export default App;
---
+++ frontend/src/components/Registerform.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Registerform.css"
+
+export default function Registerform(){
+    return (
+        <div className="flex min-h-screen bg-gray-100 p-4 items-center justify-center">
+            <div className="w-full max-w-md">
+                <form className="bg-white rounded-xl shadow-lg p-8 space-y-6">
+                    <h1 className="font-sans text-3xl font-semibold text-gray-800 mb-8">Добро пожаловать!</h1>
+
+                    <div className="space-y-4">
+                        <div>
+                            <label 
+                            htmlFor="username"
+                            className="mb-1 block text-sm font-normal text-gray-700 text-left">
+                                Логин
+                            </label>
+                            <input
+                            type="text"
+                            placeholder="Введите логин"
+                            className="px-4 py-2 border border-gray-700 rounded-lg w-full"
+                            />
+                        </div>
+
+                        <div>
+                            <label 
+                            htmlFor="username"
+                            className="mb-1 block text-sm font-normal text-gray-700 text-left">
+                                Пароль
+                            </label>
+                        <input
+                            type="text"
+                            placeholder="Введите пароль"
+                            className="px-4 py-2 border border-gray-700 rounded-lg w-full"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="space-y-4">
+                        <div>
+                            <button 
+                            type="signin"
+                            className="bg-blue-500 py-2 rounded-lg w-full
+                             text-gray-100 font-semibold text-center text-lg hover:bg-blue-600 transition">
+                                Войти
+                            </button>
+
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
