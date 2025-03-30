fa3e20ae95ed54a214cf6191ec6f08a0fea557a7
mekigiwi <danyagenij@gmail.com>
2025-03-31 01:02:19 +0300

    less cringe version of filters

--- frontend/src/pages/SchedulePage/components/ScheduleHeader.tsx
+++ frontend/src/pages/SchedulePage/components/ScheduleHeader.tsx
@@ -53,7 +53,7 @@ export default function ScheduleHeader({
   };
 
   return (
-    <div className='flex items-center gap-4 mb-4'>
+    <div className='flex items-center gap-4 mb-4 relative'>
       {/* Week tabs */}
       <div className='tabs tabs-boxed bg-base-200'>
         {[1, 2].map((week) => (
@@ -112,7 +112,7 @@ export default function ScheduleHeader({
       </div>
 
       {/* Filters dropdown */}
-      <div className='dropdown'>
+      <div className='dropdown relative'>
         <div tabIndex={0} role='button' className='btn btn-square'>
           <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' viewBox='0 0 24 24'>
             <path
@@ -175,86 +175,84 @@ export default function ScheduleHeader({
           </li>
         </ul>
       </div>
-      <div className='flex flex-col gap-2'>
-        {selectedGroup && (
-          <div className='flex flex-row gap-2'>
-            <div
-              className='btn btn-square text-gray-500 gap-2'
-              onClick={() => setSelectedGroup(null)}
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-4 w-4'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
+      <div className='relative pt-10'>
+        <div className='flex flex-col gap-2 absolute left-0 top-0 '>
+          {selectedGroup && (
+            <div className='flex flex-row gap-2'>
+              <div
+                className='btn btn-square text-gray-500 gap-2 btn-xs'
+                onClick={() => setSelectedGroup(null)}
               >
-                <line x1='18' y1='6' x2='6' y2='18'></line>
-                <line x1='6' y1='6' x2='18' y2='18'></line>
-              </svg>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-4 w-4'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  stroke='currentColor'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                >
+                  <line x1='18' y1='6' x2='6' y2='18'></line>
+                  <line x1='6' y1='6' x2='18' y2='18'></line>
+                </svg>
+              </div>
+              <div className='text-gray-500 btn btn-xs hover:bg-base-200 hover:border-base-300 cursor-default'>
+                {selectedGroup}
+              </div>
             </div>
-            <div
-              className='text-gray-500 btn hover:bg-base-200 hover:border-base-300 cursor-default'
-            >
-              {selectedGroup}
-            </div>
-          </div>
-        )}
-        {selectedTeacher && (
-          <div className='flex flex-row gap-2'>
-            <div
-              className='btn btn-square text-gray-500 gap-2'
-              onClick={() => setSelectedTeacher(null)}
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-4 w-4'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
+          )}
+          {selectedTeacher && (
+            <div className='flex flex-row gap-2'>
+              <div
+                className='btn btn-square text-gray-500 gap-2 btn-xs'
+                onClick={() => setSelectedTeacher(null)}
               >
-                <line x1='18' y1='6' x2='6' y2='18'></line>
-                <line x1='6' y1='6' x2='18' y2='18'></line>
-              </svg>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-4 w-4'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  stroke='currentColor'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                >
+                  <line x1='18' y1='6' x2='6' y2='18'></line>
+                  <line x1='6' y1='6' x2='18' y2='18'></line>
+                </svg>
+              </div>
+              <div className='text-gray-500 btn btn-xs hover:bg-base-200 hover:border-base-300 cursor-default'>
+                {selectedTeacher}
+              </div>
             </div>
-            <div
-              className='text-gray-500 btn hover:bg-base-200 hover:border-base-300 cursor-default'
-            >
-              {selectedTeacher}
-            </div>
-          </div>
-        )}
-        {selectedPlace && (
-          <div className='flex flex-row gap-2'>
-            <div
-              className='btn btn-square text-gray-500 gap-2'
-              onClick={() => setSelectedPlace(null)}
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-4 w-4'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
+          )}
+          {selectedPlace && (
+            <div className='flex flex-row gap-2'>
+              <div
+                className='btn btn-square text-gray-500 gap-2 btn-xs'
+                onClick={() => setSelectedPlace(null)}
               >
-                <line x1='18' y1='6' x2='6' y2='18'></line>
-                <line x1='6' y1='6' x2='18' y2='18'></line>
-              </svg>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='h-4 w-4'
+                  viewBox='0 0 24 24'
+                  fill='none'
+                  stroke='currentColor'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                >
+                  <line x1='18' y1='6' x2='6' y2='18'></line>
+                  <line x1='6' y1='6' x2='18' y2='18'></line>
+                </svg>
+              </div>
+              <div className='text-gray-500 btn btn-xs hover:bg-base-200 hover:border-base-300 cursor-default'>
+                {selectedPlace}
+              </div>
             </div>
-            <div className='text-gray-500 btn hover:bg-base-200 hover:border-base-300 cursor-default'>
-              {selectedPlace}
-            </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
