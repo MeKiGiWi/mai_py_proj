92f8ae7fcb3b95de0b3ee4cbab783ebff2903ade
lookingforexit <lookingforexit@yandex.com>
2025-04-05 01:27:22 +0300

    added CustomUser model to backend/mai_prod/accounts/models.py

--- backend/mai_prod/accounts/models.py
+++ backend/mai_prod/accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    
+    class Meta:
+        verbose_name = 'Custom user'
+        verbose_name_plural = 'Custom users'
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name="custom_user_groups",
+        related_query_name="custom_user",
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="custom_user_permissions",
+        related_query_name="custom_user",
+    )
+    
+    def __str__(self):
+        return self.username
