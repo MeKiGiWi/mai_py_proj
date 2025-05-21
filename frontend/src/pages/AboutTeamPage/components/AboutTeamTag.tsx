1ed5bf07ceeebaefa7ee9a18c261300e8b522310
lookingforexit <lookingforexit@yandex.com>
2025-05-21 13:52:05 +0300

    fixed AboutTeamTage link

--- frontend/src/pages/AboutTeamPage/components/AboutTeamTag.tsx
+++ frontend/src/pages/AboutTeamPage/components/AboutTeamTag.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 
 export default function AboutTeamTag() {
   return (
-    <Link to={'/aboutteam'} className='link link-hover'>
+    <Link to={'/about'} className='link link-hover'>
       <span className='text-primary'>23*73</span>
     </Link>
   );
