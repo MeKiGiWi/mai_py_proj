343faefc3e248bc4106bdd58f69fcf19c9fc91bc
lookingforexit <lookingforexit@yandex.com>
2025-05-21 13:46:46 +0300

    added lazy imports and page loading animation for SchedulePage and AboutTeamPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,32 @@
 import './App.css';
 import { Route, Routes } from 'react-router';
+import { lazy, Suspense } from 'react';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
-import SchedulePage from './pages/SchedulePage';
-import AboutTeamPage from './pages/AboutTeamPage';
+// import SchedulePage from './pages/SchedulePage';
+// import AboutTeamPage from './pages/AboutTeamPage';
 import ProtectedRoute from './components/ProtectedRoute';
 import RepeatLoginProtectedRoute from './components/RepeatLoginProtectedRoute';
+
+import PageLoader from './components/PageLoader';
+
+const SchedulePage = lazy(() => import('./pages/SchedulePage'));
+const AboutTeamPage = lazy(() => import('./pages/AboutTeamPage'));
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path='/about' element={<AboutTeamPage />} />
+
+      <Route
+        path='/about'
+        element={
+          <Suspense fallback={<PageLoader />}>
+            <AboutTeamPage />
+          </Suspense>
+        }
+      />
 
       <Route element={<RepeatLoginProtectedRoute />}>
         <Route path='/login' element={<LoginPage />} />
@@ -20,7 +34,14 @@ const App: React.FC = () => {
       </Route>
 
       <Route element={<ProtectedRoute />}>
-        <Route path='/schedule' element={<SchedulePage />} />
+        <Route
+          path='/schedule'
+          element={
+            <Suspense fallback={<PageLoader />}>
+              <SchedulePage />
+            </Suspense>
+          }
+        />
       </Route>
 
       <Route path='*' element={<p className='text-3xl'>There's nothing here: 404!</p>} />
