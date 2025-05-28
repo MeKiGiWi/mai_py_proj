fc2a00d2d7b319134f3725c07597b22f0d770337
Ivan Ivanov <vip591313@gmail.com>
2025-05-28 04:19:08 +0300

    added serializer CycledEventsSerializer

--- backend/mai_prod/schedule/serializers.py
+++ backend/mai_prod/schedule/serializers.py
@@ -1,29 +1,48 @@
 from rest_framework import serializers
-from .models import Schedule, GroupLink, Notes
+from .models import Schedule, GroupLink, Notes, CycledEvents
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = GroupLink
-        fields = ['group_name']
+        fields = ["group_name"]
+
 
 class ScheduleSerializer(serializers.ModelSerializer):
-    group_name = serializers.CharField(source='group_name.group_name')
+    group_name = serializers.CharField(source="group_name.group_name")
+
     class Meta:
         model = Schedule
         fields = [
-            'group_name',
-            'lesson_name',
-            'lesson_type',
-            'teacher',
-            'place',
-            'start_date'
+            "group_name",
+            "lesson_name",
+            "lesson_type",
+            "teacher",
+            "place",
+            "start_date",
         ]
+
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notes
         fields = [
-            'note_date',
-            'note_content',
+            "note_date",
+            "note_content",
+        ]
+
+
+class CycledEventsSerializer(serializers.ModelSerializer):
+    group_name = serializers.CharField(source="group_name.group_name")
+
+    class Meta:
+        model = Schedule
+        fields = [
+            "group_name",
+            "lesson_name",
+            "lesson_type",
+            "teacher",
+            "place",
+            "start_date",
+            "every_week",
         ]
