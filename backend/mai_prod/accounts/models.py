002514ce73c00e84f417e5fffffe61c7d3639ca2
lookingforexit <lookingforexit@yandex.com>
2025-05-28 10:40:13 +0300

    deleted useless CustomUser model, UserProfile replaced his functionality

--- backend/mai_prod/accounts/models.py
+++ backend/mai_prod/accounts/models.py
@@ -19,31 +19,3 @@ class UserProfile(models.Model):
         'credentials',
         null=True,
     )
-
-
-class CustomUser(AbstractUser):
-
-    class Meta:
-        verbose_name = 'Custom user'
-        verbose_name_plural = 'Custom users'
-
-    groups = models.ManyToManyField(
-        'auth.Group',
-        verbose_name='groups',
-        blank=True,
-        related_name="customuser_set",
-        related_query_name="customuser",
-    )
-
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        verbose_name='user permissions',
-        blank=True,
-        related_name="customuser_set",
-        related_query_name="customuser",
-    )    
-
-    credentials = models.JSONField(
-        'credentials',
-        null=True,
-    )
