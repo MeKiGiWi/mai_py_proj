9b75f85509a49ecee9a5528f3ffa9ecdb8791c09
lookingforexit <lookingforexit@yandex.com>
2025-05-28 09:10:30 +0300

    added extracting jwt token for user for authentication

--- backend/mai_prod/google_services/views.py
+++ backend/mai_prod/google_services/views.py
@@ -55,7 +55,12 @@ class GoogleAuthAPIView(APIView):
         )
 
         # getting jwt token
+        refresh = RefreshToken.for_user(user)
 
 
-
-        return Response({'google_access_token':  " aksjdfhjk"}, status=status.HTTP_201_CREATED)
+        return Response({
+                'access_token': str(refresh.access_token),
+                'refresh_token': str(refresh),
+                'user_id': user.id,
+                'username': user.username
+            }, status=status.HTTP_200_OK)
