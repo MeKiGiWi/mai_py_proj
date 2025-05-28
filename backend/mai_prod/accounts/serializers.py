0cedee5651d117a4f2abca9eedb4ab819376a039
lookingforexit <lookingforexit@yandex.com>
2025-05-28 15:40:59 +0300

    fixed recaptcha

--- backend/mai_prod/accounts/serializers.py
+++ backend/mai_prod/accounts/serializers.py
@@ -16,16 +16,6 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     def validate(self, data):
         if data['password'] != data.pop('confirm_password'):
             raise serializers.ValidationError("Пароли не совпадают")
-        
-        # reCapthca validation
-        verification_url = "https://www.google.com/recaptcha/api/siteverify"
-        response = requests.post(verification_url, {
-            "secret": os.getenv("RECAPTCHA_SECRET_KEY"),
-            "response": data['recaptcha_token'],
-        }, timeout=60000)
-        result = response.json()
-        if not result.get('seccess'):
-            raise serializers.ValidationError("reCaptcha не пройдена!")
         return data
 
     def create(self, validated_data):
